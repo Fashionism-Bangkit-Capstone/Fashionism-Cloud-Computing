25cb855f3be3ed7a53e1b5e5f72375bd08794dc4
Faza Iman Imron <cincaumyself@gmail.com>
2023-05-30 22:09:18 +0700

    feat: add preferences validator

--- app/validators/index.js
+++ app/validators/index.js
@@ -1,7 +1,9 @@
 const authValidator = require('./auth.validator');
 const profileValidator = require('./profile.validator');
+const preferenceValidator = require('./preference.validator');
 
 module.exports = {
   authValidator,
   profileValidator,
+  preferenceValidator,
 };
