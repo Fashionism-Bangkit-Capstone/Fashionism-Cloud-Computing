d798d8d588949ea711de98b54fe05fdab0e11279
Faza Iman Imron <cincaumyself@gmail.com>
2023-06-02 20:52:36 +0700

    feat: add new constants variables

--- app/utils/constants.utils.js
+++ app/utils/constants.utils.js
@@ -1,7 +1,11 @@
 const bucketPublicUrl = 'https://storage.googleapis.com';
 const avatarFolderName = 'avatars';
+const productImageFolderName = 'products';
+const apiVersion = 'v1';
 
 module.exports = {
   bucketPublicUrl,
   avatarFolderName,
+  productImageFolderName,
+  apiVersion,
 };
