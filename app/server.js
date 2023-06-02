dee7284b8e0f04d05e5c495f604b3163ef8ef0b9
Faza Iman Imron <cincaumyself@gmail.com>
2023-06-02 20:54:03 +0700

    feat: import product routes

--- app/server.js
+++ app/server.js
@@ -42,6 +42,7 @@ app.get('/', (req, res) => {
 require('./routes/auth.routes')(app);
 require('./routes/profile.routes')(app);
 require('./routes/preference.routes')(app);
+require('./routes/product.routes')(app);
 
 app.all('*', (req, res) => {
   res.status(404).send({
