f980a32fd96045696a8159f70e436a743e9cc1d2
Faza Iman Imron <cincaumyself@gmail.com>
2023-06-06 17:34:29 +0700

    feat: add favorite routes

---
+++ app/routes/favorite.routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const { authJwt } = require('../middleware');
+const { favoriteValidator } = require('../validators');
+const controller = require('../controllers/favorite.controller');
+const constants = require('../utils/constants.utils');
+
+module.exports = (app) => {
+  app.use((req, res, next) => {
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Authorization, Origin, Content-Type, Accept',
+    );
+    next();
+  });
+
+  router.get('/:user_account_id', authJwt.verifyToken, controller.index);
+  router.post(
+    '/add',
+    authJwt.verifyToken,
+    favoriteValidator.addOrRemoveFavorite,
+    controller.addFavorite,
+  );
+  router.post(
+    '/remove',
+    authJwt.verifyToken,
+    favoriteValidator.addOrRemoveFavorite,
+    controller.removeFavorite,
+  );
+
+  app.use(`/api/${constants.apiVersion}/favorites`, router);
+};
