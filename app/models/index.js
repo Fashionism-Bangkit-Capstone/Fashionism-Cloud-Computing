80a5b80ce115614a623f2d12a3c3fea72cbcaaec
Faza Iman Imron <cincaumyself@gmail.com>
2023-06-10 18:20:53 +0700

    refactor: add logging variable in model configuration

--- app/models/index.js
+++ app/models/index.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   dialect: dbConfig.dialect,
   operatorsAliases: 0,
   timestamps: 0,
+  // logging: 0,
 
   pool: {
     max: dbConfig.pool.max,
