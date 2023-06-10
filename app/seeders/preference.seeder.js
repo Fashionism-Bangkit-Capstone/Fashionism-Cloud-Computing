c6cc168fdc241f15db9d1b0389e2e396d738bd72
Faza Iman Imron <cincaumyself@gmail.com>
2023-06-10 18:21:52 +0700

    refactor: improvisation on preference seeder

--- app/seeders/preference.seeder.js
+++ app/seeders/preference.seeder.js
@@ -4,6 +4,11 @@ const Preference = db.preference;
 
 const init = async () => {
   const preferences = ['Casual', 'Street', 'Office', 'Formal'];
+
+  await Preference.destroy({ where: {} });
+  await Preference.sequelize.query(
+    'ALTER TABLE preferences AUTO_INCREMENT = 1',
+  );
   await Preference.bulkCreate(
     preferences.map((preference) => ({ name: preference })),
   );
