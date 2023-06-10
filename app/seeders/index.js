f1b81f54a5709f747ca293db16b252582fedc89f
Faza Iman Imron <cincaumyself@gmail.com>
2023-06-10 18:25:08 +0700

    feat: import msme account and product seeder

--- app/seeders/index.js
+++ app/seeders/index.js
@@ -1,7 +1,11 @@
 const preferenceSeeder = require('./preference.seeder');
+const msmeAccountSeeder = require('./msme_account.seeder');
+const productSeeder = require('./product.seeder');
 
 const init = async () => {
   await preferenceSeeder();
+  await msmeAccountSeeder();
+  await productSeeder();
 };
 
 module.exports = init;
