a02cee663d27579d27555d3cfc46ce8945190c38
Faza Iman Imron <cincaumyself@gmail.com>
2023-06-10 18:24:16 +0700

    feat: add product seeder

---
+++ app/seeders/product.seeder.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+const path = require('path').resolve('./app/seeders');
+const { Storage } = require('@google-cloud/storage');
+const db = require('../models');
+const config = require('../config/google_cloud.config');
+const constants = require('../utils/constants.utils');
+
+const storage = new Storage({ keyFilename: config.keyFilename });
+const bucket = storage.bucket(config.bucketName);
+
+const Product = db.product;
+
+const init = async () => {
+  await Product.destroy({
+    where: {},
+  });
+
+  const [files] = await bucket.getFiles({
+    prefix: `${constants.productImageFolderName}/`,
+  });
+
+  await Promise.all(
+    files.map(async (file) => {
+      await file.delete();
+    }),
+  );
+
+  const products = JSON.parse(
+    fs.readFileSync(`${path}/data/products.json`, 'utf8'),
+  );
+
+  await Promise.all(
+    products.map(async (product) => {
+      const folder = `${constants.productImageFolderName}/`;
+      const ext = product.product_image.substring(
+        product.product_image.lastIndexOf('.'),
+        product.product_image.length,
+      );
+      const fileName = `${Math.random()
+        .toString(36)
+        .substring(2, 15)}${Math.random().toString(36).substring(2, 15)}${ext}`;
+
+      const blob = bucket.file(folder + fileName);
+      const blobStream = blob.createWriteStream({
+        resumable: false,
+      });
+
+      blobStream.on('error', (err) => console.log(err));
+
+      blobStream.on('finish', async () => {
+        try {
+          await bucket.file(folder + fileName).makePublic();
+        } catch (err) {
+          console.log(err);
+        }
+      });
+
+      blobStream.end(
+        fs.readFileSync(`${path}/data/raw/products/${product.product_image}`),
+      );
+
+      await Product.create({
+        name: product.name,
+        description: product.description,
+        stock: product.stock,
+        price: product.price,
+        product_image: fileName,
+        msme_account_id: product.msme_account_id,
+      });
+    }),
+  );
+};
+
+module.exports = init;
