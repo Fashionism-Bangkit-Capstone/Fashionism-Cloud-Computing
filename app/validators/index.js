dfcfe7285551a9ab4c0d27eb2baff6398863c241
Faza Iman Imron <cincaumyself@gmail.com>
2023-06-11 20:11:15 +0700

    delete everything related to preferences

--- app/validators/index.js
+++ app/validators/index.js
@@ -1,13 +1,11 @@
 const authValidator = require('./auth.validator');
 const profileValidator = require('./profile.validator');
-const preferenceValidator = require('./preference.validator');
 const productValidator = require('./product.validator');
 const favoriteValidator = require('./favorite.validator');
 
 module.exports = {
   authValidator,
   profileValidator,
-  preferenceValidator,
   productValidator,
   favoriteValidator,
 };
