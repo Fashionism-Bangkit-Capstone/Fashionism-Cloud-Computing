4c0266464af821db945c635ba0d9f3b0c40485be
Faza Iman Imron <cincaumyself@gmail.com>
2023-06-11 20:00:34 +0700

    feat: add category routes

---
+++ app/routes/category.routes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const { authJwt } = require('../middleware');
+const controller = require('../controllers/category.controller');
+const constants = require('../utils/constants.utils');
+
+module.exports = (app) => {
+  app.use((req, res, next) => {
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Authorization, Origin, Content-Type, Accept',
+    );
+    next();
+  });
+
+  router.get('/', authJwt.verifyToken, controller.index);
+
+  app.use(`/api/${constants.apiVersion}/category`, router);
+};
